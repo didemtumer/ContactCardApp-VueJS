3bf4e3c8d173f76e888f2e97bf128fe628aba242
didedizman <didemdizman@gmail.com>
2021-01-27 15:51:22 +0300

    ContactCardApp
    V-1

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import HomePage from "@/views/HomePage";
+import ContactRequests from "@/views/ContactRequests";
+import MyContacts from "@/views/MyContacts";
+import NotFoundPage from "@/views/NotFoundPage";
+
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    component: HomePage
+  },
+  {
+    path: "/contact-requests",
+    component: ContactRequests
+  },
+  {
+    path: "/contacts",
+    component: MyContacts
+  }
+];
+
+const router = createRouter({
+  // history: createWebHistory(),
+  history: createWebHashHistory(),
+  routes
+});
+
+export default router;
